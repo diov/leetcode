4b5e89f975a16c47ec198032159879f46b525141
diov <diov87@outlook.com>
2019-02-20 20:42:36 +0800

    599 Plus One

--- Array/727-RemoveDuplicatesFromSortedArray.kt
+++ Array/727-RemoveDuplicatesFromSortedArray.kt
@@ -11,7 +11,7 @@ class Solution {
     fun removeDuplicates(nums: IntArray): Int {
         val count = nums.count()
         if (count < 2) return count
-        
+
         var result = 0
         for (index in 1 until nums.count()) {
             if (nums[index] != nums[index - 1]) {
