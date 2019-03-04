ff89485584a451d99211efe846153235383e60dc
Dio.Ye <diov87@outlook.com>
2019-03-04 14:52:10 +0800

    885 Implement strStr

---
+++ String/885-ImplementStrStr.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    val haystack = "a"
+    val needle = "a"
+    println(Solution().strStr(haystack, needle))
+}
+
+class Solution {
+    fun strStr(haystack: String, needle: String): Int {
+        val needleSize = needle.count()
+        if (needleSize == 0) return 0
+        val needleFirst = needle[0]
+
+        haystack.forEachIndexed { index, c ->
+            if (c == needleFirst) {
+                if (index + needleSize > haystack.count()) {
+                    return -1
+                }
+                if (haystack.subSequence(index, index + needleSize) == needle) {
+                    return index
+                }
+            }
+        }
+        return -1
+    }
+}
