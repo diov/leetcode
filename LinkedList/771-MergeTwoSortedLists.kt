db42ade239745011b12f7b3bf6188a7cdd004e0d
Dio.Ye <diov87@outlook.com>
2019-03-09 19:07:59 +0800

    771 Merge Two Sorted Lists

---
+++ LinkedList/771-MergeTwoSortedLists.kt
@@ -0,0 +1,65 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+    
+    fun printList() {
+        println(`val`)
+        var nextNode = next
+        while(null != nextNode) {
+            println(nextNode.`val`)
+            nextNode = nextNode.next
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val l1values = intArrayOf(1,2,3,4)
+    val l1 = ListNode(0)
+    var current1: ListNode? = l1
+    l1values.forEachIndexed { index, i ->
+        if (index == 0) {
+            l1.`val` = i
+        } else {
+            current1?.next = ListNode(i)
+            current1 = current1?.next
+        }
+    }
+    val l2values = intArrayOf(5,6,7,8)
+    val l2 = ListNode(0)
+    var current2: ListNode? = l2
+    l2values.forEachIndexed { index, i ->
+        if (index == 0) {
+            l2.`val` = i
+        } else {
+            current2?.next = ListNode(i)
+            current2 = current2?.next
+        }
+    }
+    Solution().mergeTwoLists(l1, l2)?.printList()
+}
+
+class Solution {
+    fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+        if (null == l1) return l2
+        if (null == l2) return l1
+        
+        var head: ListNode
+        if (l1.`val` > l2.`val`) {
+            head = l2
+            head.next = mergeTwoLists(l1, l2.next)
+        } else {
+            head = l1
+            head.next = mergeTwoLists(l1.next, l2)
+        }
+        return head
+    }
+}
